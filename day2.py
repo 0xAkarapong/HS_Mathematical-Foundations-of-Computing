7f579904cea91c86f923d82fe9cef8117800231c
akarapong <97168241+0xAkarapong@users.noreply.github.com>
2025-02-04 19:34:33 +0700

    implement matplotlib

--- day2.py
+++ day2.py
@@ -8,22 +8,18 @@ def f(x):
 def bisection_interval_subdivisions(a, b, iterations=1000000, tolerance=1e-6) -> list:
     roots = []
     if f(a) * f(b) >= 0:
-        print("Function values at endpoints do not have opposite signs in interval [",a,b,"].")
         return roots
 
-    while iterations > 0:
+    for _ in range(iterations):
         c = (a + b) / 2
         if abs(f(c)) < tolerance:
             roots.append(c)
             return roots
-            
+
         if f(a) * f(c) < 0:
             b = c
         else:
             a = c
-        iterations -= 1
-
-    print("Maximum iterations reached in interval [",a,b,"].")
     return roots
 
 def find_all_roots(start, end, step, iterations=1000000, tolerance=1e-6):
@@ -31,34 +27,39 @@ def find_all_roots(start, end, step, iterations=1000000, tolerance=1e-6):
   current_interval_start = start
   while current_interval_start < end:
       current_interval_end = current_interval_start + step
-      
+
       new_roots = bisection_interval_subdivisions(current_interval_start,current_interval_end,iterations,tolerance)
       if(len(new_roots) > 0):
-        if new_roots[0] not in all_roots :
-          all_roots.extend(new_roots)
+        root = new_roots[0]
+        is_duplicate = False
+        for existing_root in all_roots:
+            if abs(root - existing_root) < tolerance:
+                is_duplicate = True
+                break
+        if not is_duplicate:
+          all_roots.append(root)
 
       current_interval_start = current_interval_end
   return all_roots
-  
+
 all_roots_found = find_all_roots(-3, 3, 0.1)
 print("All roots found:", all_roots_found)
 
-# Plotting
 x = np.linspace(-3, 3, 400)
-y = f(x) 
+y = f(x)
 
 plt.figure(figsize=(10, 6))
-plt.plot(x, y, label='f(x) = x^4 + 3x^3 + x^2 - 2x - 0.5')
-plt.axhline(0, color='black',linewidth=0.5)  # 
-plt.axvline(0, color='black',linewidth=0.5) 
-plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5) # Add grid
+plt.plot(x, y, label='f(x) = x^4 + 3x^3 + x**2 - 2x - 0.5')
+plt.axhline(0, color='black',linewidth=0.5)
+plt.axvline(0, color='black',linewidth=0.5)
+plt.grid(color = 'gray', linestyle = '--', linewidth = 0.5)
 
-# Plot roots
 for root in all_roots_found:
-    plt.plot(root, 0, 'ro', label='Root' if root == all_roots_found[0] else "") 
+    plt.plot(root, 0, 'ro', label='Root' if root == all_roots_found[0] else "")
 
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.title('Graph of f(x) and its Roots')
 plt.legend()
+plt.ylim([-5, 10]) 
 plt.show()
