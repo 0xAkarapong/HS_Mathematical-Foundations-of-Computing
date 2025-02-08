8c20e2e148248eca2974a12f33ab228963cbd46c
akarapong <97168241+0xAkarapong@users.noreply.github.com>
2025-02-08 17:52:20 +0700

    Enhance app functionality: update HTML title and add input field for mathematical expressions; modify app.py imports for improved symbol handling

--- week1homework/app.py
+++ week1homework/app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
-import sympy as sym
+from flask import Flask, render_template, request
+from sympy import symbols, sympify
+import numpy as np
+
+x = symbols('x')
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("home.html")
+    if request.method == 'POST':
+        expression = request.form['expression']
+        return render_template("home.html", expression=expression)
 
 if __name__ == "__main__":
     app.run(debug=True)
